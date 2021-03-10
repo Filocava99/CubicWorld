aa0c20064c62f13feb7f01acd49056a21fff0aac
Filippo Cavallari <filippo.cavallari99@gmail.com>
2021-03-10 16:22:27 +0100

    Created math function to calculate a surface normal given three vertices of a triangle

--- src/main/kotlin/it/filippocavallari/lwge/math/Math.kt
+++ src/main/kotlin/it/filippocavallari/lwge/math/Math.kt
@@ -5,7 +5,14 @@ import org.joml.Vector3f
 
 object Math {
 
-    fun calculateNormalTangents(pos1: Vector3f, pos2: Vector3f, pos3: Vector3f, uv1: Vector2f, uv2: Vector2f, uv3: Vector2f): NormalTangents{
+    fun calculateNormalTangents(
+        pos1: Vector3f,
+        pos2: Vector3f,
+        pos3: Vector3f,
+        uv1: Vector2f,
+        uv2: Vector2f,
+        uv3: Vector2f
+    ): NormalTangents {
         val edge1 = Vector3f(pos2).sub(pos1)
         val edge2 = Vector3f(pos3).sub(pos1)
         val deltaUV1 = Vector2f(uv2).sub(uv1)
@@ -26,4 +33,14 @@ object Math {
         return NormalTangents(tangent, biTangent)
     }
 
+    fun calculateSurfaceNormal(v1: Vector3f, v2: Vector3f, v3: Vector3f): Vector3f {
+        //https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
+        val U = Vector3f(v2).sub(v1)
+        val V = Vector3f(v3).sub(v1)
+        val normal = Vector3f()
+        normal.x = U.y*V.z-U.z*V.y
+        normal.y = U.z*V.x-U.x*V.z
+        normal.z = U.x*V.y-U.y*V.x
+        return normal
+    }
 }
